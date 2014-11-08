015891089bb1abfacd4ee712699c3ecd335f9f05
yu yang <yundkyy@gmail.com>
2014-11-08 23:35:05 +0800

    bower 路径加入到可以静态访问中

--- example/app.js
+++ example/app.js
@@ -31,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use('/bower', express.static(__dirname + '/bower_components'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
