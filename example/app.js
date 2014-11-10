9d5ed4b62181ecd4b13adcab462c80637759f314
yu yang <yundkyy@gmail.com>
2014-11-10 17:22:06 +0800

    跨域 host增加 和  传回对象。前端能直接使用。

--- example/app.js
+++ example/app.js
@@ -22,6 +22,18 @@ app.set('views', __dirname + '/views');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+//设置跨域
+/*
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    // res.header("X-Powered-By",' 3.2.1');
+    // res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+*/
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- example/routes/index.js
+++ example/routes/index.js
@@ -9,4 +9,9 @@ router.get('/haha/:age', function(req, res) {
 	console.log(req.params.age);
   res.render('index', { name: req.params.age});
 });
+router.get('/rest', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
+	// console.log(req.params.age);
+  res.send({name:"yangyu",age:25});
+});
 module.exports = router;
